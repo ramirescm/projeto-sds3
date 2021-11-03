31c03ce32b00accd18c20f599c724682a16c9538
ramirescm <ramirescm@gmail.com>
2021-11-03 20:24:01 -0300

    Group by search

--- backend/src/main/java/com/devsuperior/dsvendas/controllers/SaleController.java
+++ backend/src/main/java/com/devsuperior/dsvendas/controllers/SaleController.java
@@ -3,24 +3,40 @@ package com.devsuperior.dsvendas.controllers;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.devsuperior.dsvendas.dto.SellerDTO;
-import com.devsuperior.dsvendas.services.SellerService;
+import com.devsuperior.dsvendas.dto.SaleDTO;
+import com.devsuperior.dsvendas.dto.SaleSuccessDTO;
+import com.devsuperior.dsvendas.dto.SaleSumDTO;
+import com.devsuperior.dsvendas.services.SaleService;
 
 @RestController
-@RequestMapping(value = "/sellers")
+@RequestMapping(value = "/sale")
 public class SaleController {
 
 	@Autowired
-	private SellerService service;
-	
+	private SaleService service;
+
 	@GetMapping
-	public ResponseEntity<List<SellerDTO>> getAll(){
-		List<SellerDTO> list = service.findAll();
+	public ResponseEntity<Page<SaleDTO>> getAll(Pageable pageable) {
+		Page<SaleDTO> list = service.findAll(pageable);
+		return ResponseEntity.ok(list);
+	}
+
+	@GetMapping(value = "amount-by-seller")
+	public ResponseEntity<List<SaleSumDTO>> amountGroupedBySeller() {
+		List<SaleSumDTO> list = service.amountGroupedBySeller();
+		return ResponseEntity.ok(list);
+	}
+
+	@GetMapping(value = "success-by-seller")
+	public ResponseEntity<List<SaleSuccessDTO>> successGroupedBySeller() {
+		List<SaleSuccessDTO> list = service.successGroupedBySeller();
 		return ResponseEntity.ok(list);
 	}
 }
--- backend/src/main/java/com/devsuperior/dsvendas/repositories/SaleRepository.java
+++ backend/src/main/java/com/devsuperior/dsvendas/repositories/SaleRepository.java
@@ -1,9 +1,21 @@
 package com.devsuperior.dsvendas.repositories;
 
-import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.List;
 
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import com.devsuperior.dsvendas.dto.SaleSuccessDTO;
+import com.devsuperior.dsvendas.dto.SaleSumDTO;
 import com.devsuperior.dsvendas.entities.Sale;
 
 public interface SaleRepository extends JpaRepository<Sale, Long> {
 
+	@Query("SELECT new com.devsuperior.dsvendas.dto.SaleSumDTO(s.seller, SUM(s.amount)) "
+			+ " FROM Sale AS s GROUP BY s.seller")
+	List<SaleSumDTO> amountGroupedBySeller();
+	
+	@Query("SELECT new com.devsuperior.dsvendas.dto.SaleSuccessDTO(s.seller, SUM(s.visited), SUM(s.deals)) "
+			+ " FROM Sale AS s GROUP BY s.seller")
+	List<SaleSuccessDTO> successGroupedBySeller();
 }
